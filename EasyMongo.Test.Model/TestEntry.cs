f0689c52cf5d1cbadc16ac9e092e16597ddbaf3c
TheFastCat <thefastcat@hotmail.com>
2013-10-18 08:51:42 -0600

    Converting EasyMongo non-test assemblies to strongly named using encrypted key files.

--- EasyMongo.Test.Model/TestEntry.cs
+++ EasyMongo.Test.Model/TestEntry.cs
@@ -11,6 +11,13 @@ using EasyMongo.Contract;
 
 namespace EasyMongo.Test.Model
 {
+    /// <summary>
+    /// This class exists for unit testing purposes to replace an usage instance of an object that will 
+    /// be stored/retrieved from the MongoDB. It exists in a separate assembly from the TestFixtures in order
+    /// to be bound via a Ninject module to the objects in the TestFixtures
+    /// </summary>
+    /// TODO - Investigate removal of EasyMongo.Test.Model & TestEntry ?? why is this currently needed after refactoring
+    /// of EasyMongo classes to not take a model object at construction time? I think it can be removed...
     public class TestEntry : IEasyMongoEntry
     {
         public TestEntry()
