90b241889ab1f76f1c127a4f379768dc92b4cd8d
TheFastCat <thefastcat@hotmail.com>
2014-08-02 22:30:14 -0600

    Changes required to pass UpdaterTaskTTest.FindAndModifyTest1

--- EasyMongo.Async.Test/UpdaterTaskTTest.cs
+++ EasyMongo.Async.Test/UpdaterTaskTTest.cs
@@ -237,11 +237,12 @@ namespace EasyMongo.Async.Test
             findAndModifyArgs.Upsert = true;
             findAndModifyArgs.VersionReturned = FindAndModifyDocumentVersion.Modified;
 
-            _updaterTaskT.FindAndModifyAsync(MONGO_COLLECTION_1_NAME, findAndModifyArgs);
+            FindAndModifyResult findAndModifyResult = await _updaterTaskT.FindAndModifyAsync(MONGO_COLLECTION_1_NAME, findAndModifyArgs);
 
-            Assert.IsTrue(_findAndModifyResult.Ok, "FindAndModifyResult from FindAndModify not OK");
-            Assert.IsNull(_findAndModifyResult.ErrorMessage);
-            Assert.IsNotNull(_findAndModifyResult.ModifiedDocument);
+            Assert.IsTrue(findAndModifyResult.Ok);
+            Assert.IsNotNull(findAndModifyResult.Command);
+            Assert.IsNull(findAndModifyResult.ErrorMessage);
+            Assert.IsNotNull(findAndModifyResult.ModifiedDocument);
 
             results = new List<Entry>(_readerT.Read(MONGO_COLLECTION_1_NAME, "TimeStamp", _beforeTest, DateTime.Now));
             Assert.AreEqual(1, results.Count());
